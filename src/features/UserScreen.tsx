22bcfa4d3249050d96d46beca17f9b48af9fc762
anuragalakkat98 <92522891+anuragalakkat98@users.noreply.github.com>
2022-07-22 03:29:43 +0530

    Add files via upload

---
+++ src/features/UserScreen.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { Col, Row } from "react-bootstrap";
+import { UserManager } from "../manager/UserManager";
+import PostScreen from "./PostScreen";
+import UserList from "./UserList";
+
+const UserScreen = () => {
+  const loadAllUsers = async () => {
+    await UserManager.getAllUsers();
+  };
+
+  useEffect(() => {
+    loadAllUsers();
+  }, []);
+
+  return (
+    <div>
+      <Row>
+        <Col sm={4}>
+          <UserList />
+        </Col>
+        <Col sm={8}>
+          <PostScreen />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default UserScreen;
